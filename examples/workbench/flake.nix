7dd9f8c640e52f80025b867414a92a49c92ebf82
Natalia Piotrowski <n.piotrowski@tum.de>
2026-06-25 15:18:32 +0200

    work on getting quiche example to work

---
+++ examples/workbench/flake.nix
@@ -0,0 +1,79 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixnet.url = "github:birneee/nixnet";
+    quiche.url = "git+https://github.com/Nadel1/quiche?ref=further-research-plateau";#has to look like this as quiche uses submodules
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixnet.supportedSystems;
+      perSystem =
+        { inputs', pkgs, ... }:
+        let
+          nixnet = inputs'.nixnet.legacyPackages;
+          config = {
+            arp = false;
+            arpPrefill = true;
+            #preRun = ''
+            #  python3 ../../space_quic/workbench/analysis/setup.py ../../space_quic/workbench/network/experiments/1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json
+            #'';
+            nodePackages = with pkgs; [
+              inputs'.quiche.packages.default
+              coreutils
+              iputils
+            ];
+            nodes = {
+              server = {
+                networking.interfaces.veth0.ipv4.addresses = [
+                  {
+                    address = "10.0.0.2";
+                    prefixLength = 24;
+                  }
+                ];
+                scripts.main={
+                  exec="tokio-server --listen 10.0.0.2:4433 --root ./ --cert quiche/tokio-quiche/src/bin/cert.crt --key quiche/tokio-quiche/src/bin/cert.key --initial-rtt 1000 --idle-timeout 100000 --cc-algorithm bbr2";# --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T15:52:53.419682-Bob-CR/quiche:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:server0:bbr2:run=0:baseline.csv";
+                  # --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T14:11:20.310501-Bob-CR/quinn:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:server0:bbr:run=0:baseline.csv";
+                };
+                workDir="./server";
+              };
+              client = {
+                networking.interfaces.veth0.ipv4.addresses = [
+                  {
+                    address = "10.0.0.1";
+                    prefixLength = 24;
+                  }
+                ];
+                scripts.main = {
+                  exec = "tokio-client --no-verify http://10.0.0.2:4433/README.md --initial-rtt 1000 --idle-timeout 100000 --cc-algorithm bbr2";# --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T15:52:53.419682-Bob-CR/quiche:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:client0:bbr2:run=0:baseline.csv --requests 1";
+                  await= true;
+                };
+                workDir="./client";
+              };
+              
+              
+            };
+
+          };
+          veths.veth0 = {
+              arpPrefill = true;
+              arp = false;
+              mtu = 1500;
+              netem = {
+                rateMbit = 1000;
+                delayMs = 50;
+                autoLimit = true;
+              };
+              a.node = "client";
+              b.node = "server";
+            };
+        in
+        {
+          packages.default = nixnet.mkExperiment config;
+          packages.mermaid = nixnet.mkMermaid config;
+          packages.mermaid-svg = nixnet.mkMermaidSvg config;
+        };
+    };
+}
