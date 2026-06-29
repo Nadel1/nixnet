42b3d6b6b69e74acb0895206a7ef68f28603bdfc
NataliaPiotrowski <n.piotrowski@tum.de>
2026-06-29 16:55:06 +0200

    newest nix files

--- examples/workbench/flake.nix
+++ examples/workbench/flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixnet.url = "github:birneee/nixnet";
+    test-certs.url = "github:Nadel1/test-certs";
     quiche.url = "git+https://github.com/Nadel1/quiche?ref=further-research-plateau";#has to look like this as quiche uses submodules
   };
 
@@ -26,19 +27,6 @@
               iputils
             ];
             nodes = {
-              server = {
-                networking.interfaces.veth0.ipv4.addresses = [
-                  {
-                    address = "10.0.0.2";
-                    prefixLength = 24;
-                  }
-                ];
-                scripts.main={
-                  exec="tokio-server --listen 10.0.0.2:4433 --root ./ --cert quiche/tokio-quiche/src/bin/cert.crt --key quiche/tokio-quiche/src/bin/cert.key --initial-rtt 1000 --idle-timeout 100000 --cc-algorithm bbr2";# --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T15:52:53.419682-Bob-CR/quiche:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:server0:bbr2:run=0:baseline.csv";
-                  # --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T14:11:20.310501-Bob-CR/quinn:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:server0:bbr:run=0:baseline.csv";
-                };
-                workDir="./server";
-              };
               client = {
                 networking.interfaces.veth0.ipv4.addresses = [
                   {
@@ -52,6 +40,20 @@
                 };
                 workDir="./client";
               };
+              server = {
+                networking.interfaces.veth0.ipv4.addresses = [
+                  {
+                    address = "10.0.0.2";
+                    prefixLength = 24;
+                  }
+                ];
+                scripts.main={
+                  exec="tokio-server --listen 10.0.0.2:4433 --root ./ --cert  ${inputs'.test-certs.packages.default}/cert.crt --key ${inputs'.test-certs.packages.default}/cert.key --initial-rtt 1000 --idle-timeout 100000 --cc-algorithm bbr2";# --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T15:52:53.419682-Bob-CR/quiche:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:server0:bbr2:run=0:baseline.csv";
+                  # --logging-file /home/natia/uni/space_quic/workbench/runs/2026-06-24T14:11:20.310501-Bob-CR/quinn:1-2-1-rtt=1s-rate=3125000Bps-loss=0%-asymFactor=1-outageduration-0.json:server0:bbr:run=0:baseline.csv";
+                };
+                workDir="./server";
+              };
+              
               
               
             };
